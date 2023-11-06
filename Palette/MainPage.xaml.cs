59ce6e47b45dee23b21ceba7704d6fa8ab4258e1
Aarón Flores Pasos <aaron.flores7908@alumnos.udg.mx>
2023-11-06 02:14:12 -0600

    Add: Method to change random BackgroundColor of the Container, Title and Hex

--- Palette/MainPage.xaml.cs
+++ Palette/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace Palette
@@ -11,7 +12,9 @@ namespace Palette
 	public partial class MainPage : ContentPage
 	{
 		//Global attributes
-		private Color color;
+		private Color color = new Color();
+		private Random random = new Random();
+		private bool IsRandom = false;
 
 		public MainPage()
 		{
@@ -27,26 +30,50 @@ namespace Palette
 
 		private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
 		{
-			var Red = SldRed.Value;
-			var Green = SldGreen.Value;
-			var Blue = SldBlue.Value;
-			var Alpha = SldTransparency.Value;
+            if (!IsRandom)
+            {
+				var Red = SldRed.Value;
+				var Green = SldGreen.Value;
+				var Blue = SldBlue.Value;
+				var Alpha = SldTransparency.Value;
 
-			color = Color.FromRgba(Red, Green, Blue, Alpha);
+				color = Color.FromRgba(Red, Green, Blue, Alpha);
 
+				SetColor(color);
+			}
+        }
+
+		private void SetColor(Color color)
+		{
 			TxtRed.Text = color.R.ToString();
 			TxtGreen.Text = color.G.ToString();
 			TxtBlue.Text = color.B.ToString();
 			TxtTransparency.Text = color.A.ToString();
 
-			SetColor(color);
-		}
-
-		private void SetColor(Color color)
-		{
 			Container.BackgroundColor = color;
+			LblTitle.TextColor = color;
 			LblHex.TextColor = color;
 			LblHex.Text = "Hex value: " + color.ToHex();
+		}
+
+		private void Random_Clicked(object sender, EventArgs e)
+		{
+			IsRandom = true;
+
+            color = Color.FromRgba(
+				random.Next(0, 256),
+				random.Next(0, 256),
+				random.Next(0, 256),
+				random.Next(0,255));
+
+			SetColor(color);
+
+			SldRed.Value = color.R;
+			SldGreen.Value = color.G;
+			SldBlue.Value = color.B;
+			SldTransparency.Value = color.A;
+
+			IsRandom = false;
 		}
 	}
 }
