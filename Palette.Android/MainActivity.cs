788f2e3ad26379e1e71f96c1ddc9a68d613307e6
Aarón Flores Pasos <aaron.flores7908@alumnos.udg.mx>
2023-11-05 19:53:49 -0600

    Add: Frame component and styles (Hex value)

--- Palette.Android/MainActivity.cs
+++ Palette.Android/MainActivity.cs
@@ -4,17 +4,22 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using FFImageLoading.Svg.Forms;
 
 namespace Palette.Droid
 {
     [Activity(Label = "Palette", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+		protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+			// Initialize FFImageLoading
+			FFImageLoading.Forms.Platform.CachedImageRenderer.Init(enableFastRenderer: true);
+			var ignore = typeof(SvgCachedImage);
+
+			Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
